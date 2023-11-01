ced520090b9c627445035523f1350a53487fda35
양유경 <gosjorb@gmail.com>
2023-11-01 16:56:08 +0900

    feat: create spring boot project (#2)

---
+++ src/main/java/com/ssafy/tripoline/TripolineApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.tripoline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripolineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripolineApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/ssafy/tripoline/TripolineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.tripoline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripolineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
