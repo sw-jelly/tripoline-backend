2be9af1729c5035dfa4af4a9ddd4f9d97ffcaf3c
지인성 <crm03008@gmail.com>
2023-11-03 17:03:53 +0900

    #5 basic Controller, ControllerAdvice Setting

--- src/main/java/com/ssafy/tripoline/TripolineApplication.java
+++ src/main/java/com/ssafy/tripoline/TripolineApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.tripoline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.ssafy"})
 public class TripolineApplication {
 
 	public static void main(String[] args) {
